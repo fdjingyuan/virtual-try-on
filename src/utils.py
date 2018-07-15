9c4467082e2c82ce55a8ea934be2a78f541632bb
jingyuanliu <fdjingyuan@outlook.com>
2018-07-14 22:28:41 -0400

    merge

--- src/utils.py
+++ src/utils.py
@@ -33,6 +33,7 @@ def get_train_test():
     # 前80%item_id，并转dataframe
     train_id = pd.DataFrame(item_id.iloc[:int(len(item_id) * 0.8)])
     train_df = pd.merge(df, train_id, on='item_id')
+    #merge即join，默认为inner join
     # 后20%item_id，并转dataframe
     test_id = pd.DataFrame(item_id.iloc[int(len(item_id) * 0.8):])
     test_df = pd.merge(df, test_id, on='item_id')
